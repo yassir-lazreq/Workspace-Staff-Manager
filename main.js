b1d96a0df7cc6e4a6299b59887fd7ce15547b2b2
Yassir Lazreq <yassirlazre@gmail.com>
2025-11-18 17:16:43 +0100

    feat: Enhance employee form submission with experience details and input adjustments

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const addBtn = document.getElementById("ajouter");
 const addContainer = document.getElementById("addContainer");
 const modalOverlay = document.querySelector(".modal-overlay");
+const formulaire = document.getElementById("formulaire");
+const addExperianceBtn = document.getElementById("addExperianceBtn");
 
+let experienceCount = 1;
+let employeeArray = [];
 
 addBtn.addEventListener('click',() =>{
     addContainer.className = "add-container";
@@ -9,3 +13,35 @@ addBtn.addEventListener('click',() =>{
 modalOverlay.addEventListener('click',() =>{
     addContainer.className = "hidden";
 })
+
+formulaire.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const nom = document.getElementById("nom").value;
+    const role = document.getElementById("role").value;
+    const email = document.getElementById("email").value;
+    const telephone = document.getElementById("telephone").value;
+    const img = document.getElementById("img").files[0];
+    const experience = document.getElementById("Experience").value;
+    const post = document.getElementById("post").value;
+    const periode = document.getElementById("periode").value;
+    const department = null;
+
+    const employee = {
+        nom: nom,
+        role: role,
+        email: email,
+        telephone: telephone,
+        img: img,
+        experience: experience,
+        post: post,
+        periode: periode,
+        department: department
+    }
+    employeeArray.push({
+        employee
+    });
+
+    addContainer.className = "hidden";
+    console.log(employeeArray);
+    formulaire.reset();
+});
