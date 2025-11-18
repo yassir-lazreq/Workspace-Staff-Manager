76f3ff4fdc7cb5edad72c99eceec241ae32adcaa
Yassir Lazreq <yassirlazre@gmail.com>
2025-11-18 18:16:53 +0100

    feat: Add experience section to employee form with dynamic input fields

--- main.js
+++ main.js
@@ -7,23 +7,56 @@ const addExperianceBtn = document.getElementById("addExperianceBtn");
 let experienceCount = 1;
 let employeeArray = [];
 
-addBtn.addEventListener('click',() =>{
+addBtn.addEventListener('click', () => {
     addContainer.className = "add-container";
 })
-modalOverlay.addEventListener('click',() =>{
+modalOverlay.addEventListener('click', () => {
     addContainer.className = "hidden";
 })
 
-formulaire.addEventListener('submit',(e) =>{
+addExperianceBtn.addEventListener('click', () => {
+    experienceCount++;
+    const experiencesDiv = document.querySelector(".list-des-experience");
+
+    const newExperienceDiv = document.createElement("div");
+    newExperienceDiv.classList = "experiences-div";
+    experiencesDiv.appendChild(newExperienceDiv);
+
+    const newEntreprise = document.createElement("input");
+    newEntreprise.classList = "input-experience";
+    newEntreprise.placeholder = "Entreprise";
+
+    const newPost = document.createElement("input");
+    newPost.classList = "input-experience";
+    newPost.placeholder = "Poste";
+
+    const newDuree = document.createElement("input");
+    newDuree.classList = "input-experience";
+    newDuree.placeholder = "Période (ex: 2020 - 2025)";
+
+    newExperienceDiv.appendChild(newEntreprise);
+    newExperienceDiv.appendChild(newPost);
+    newExperienceDiv.appendChild(newDuree);
+    console.log(newExperienceDiv)
+})
+
+formulaire.addEventListener('submit', (e) => {
     e.preventDefault();
+    let experiencesArray = [];
+    
     const nom = document.getElementById("nom").value;
     const role = document.getElementById("role").value;
     const email = document.getElementById("email").value;
     const telephone = document.getElementById("telephone").value;
     const img = document.getElementById("img").files[0];
-    const experience = document.getElementById("Experience").value;
-    const post = document.getElementById("post").value;
-    const periode = document.getElementById("periode").value;
+
+    const experience = {
+        entreprise: document.getElementById("Experience").value,
+        poste: document.getElementById("Poste").value,
+        duree: document.getElementById("Duree").value
+    }
+    experiencesArray.push(experience);
+
     const department = null;
 
     const employee = {
@@ -32,14 +65,10 @@ formulaire.addEventListener('submit',(e) =>{
         email: email,
         telephone: telephone,
         img: img,
-        experience: experience,
-        post: post,
-        periode: periode,
+        experience: experiencesArray,
         department: department
     }
-    employeeArray.push({
-        employee
-    });
+    employeeArray.push(employee);
 
     addContainer.className = "hidden";
     console.log(employeeArray);
